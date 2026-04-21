540507bdeb81f27a281c7aae5623e403afadec8d
muratbekov3 <akylbekmuratbekuulu@gmail.com>
2026-04-21 17:15:38 +0200

    test

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('DevSecOps Project Running 🚀');
+  res.send('DevSecOps Project Running without Vuln🚀');
 });
 
 app.listen(3000, () => {
